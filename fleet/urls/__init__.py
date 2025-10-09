40ac7c1b3e2f391607fcc28a9a52bd0ba60a2d57
Infelo <infelo453@gmail.com>
2025-10-10 01:46:19 +0545

    changes

--- fleet/urls/__init__.py
+++ fleet/urls/__init__.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('fleet.urls.vehicle_urls')),
     path('share-track/', include('fleet.urls.share_track_urls')),
+    path('', include('fleet.urls.vehicle_urls')),
 ]
