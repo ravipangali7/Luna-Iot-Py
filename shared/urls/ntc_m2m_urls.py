2193d3311ceea575b262e028f2c8c909805f6bad
Infelo <infelo453@gmail.com>
2025-12-30 03:05:22 +0545

    Chagesn

--- shared/urls/ntc_m2m_urls.py
+++ shared/urls/ntc_m2m_urls.py
@@ -6,7 +6,5 @@ from shared.views import ntc_m2m_views
 
 urlpatterns = [
     path('ntc-m2m/fetch-report/', ntc_m2m_views.fetch_ntc_m2m_report, name='fetch_ntc_m2m_report'),
-    path('ntc-m2m/report-status/<str:task_id>/', ntc_m2m_views.get_ntc_m2m_report_status, name='get_ntc_m2m_report_status'),
-    path('ntc-m2m/report-status/<str:task_id>', ntc_m2m_views.get_ntc_m2m_report_status, name='get_ntc_m2m_report_status_no_slash'),
 ]
 
